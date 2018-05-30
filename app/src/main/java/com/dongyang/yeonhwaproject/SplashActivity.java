c81e070db6601d884fe1109dbe178eebb2b482ec
limsu <kkjjgf@naver.com>
2018-05-30 21:14:17 +0900

    splash

---
+++ app/src/main/java/com/dongyang/yeonhwaproject/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.dongyang.yeonhwaproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+                overridePendingTransition(R.anim.right_in_animation, R.anim.not_move_animation);
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
